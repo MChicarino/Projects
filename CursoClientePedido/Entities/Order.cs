ad83d0236c33fc605112662d7975f8c04ac84c35
marcio.chicarino <marcio.chicarino@rustcon.com.br>
2019-08-26 21:28:48 -0300

    primeira versão do projeto

---
+++ CursoClientePedido/Entities/Order.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Text;
+using CursoClientePedido.Entities.Enums;
+
+
+namespace CursoClientePedido.Entities
+{
+    class Order
+    {
+        public DateTime Moment { get; set; } = DateTime.Now;
+        public OrderStatus Status { get; set; }
+        public Client Client { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+
+        public Order()
+        {
+
+        }
+
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+        public double Total()
+        {
+            double tot = 0;
+            foreach (OrderItem item in Items)
+            {
+                {
+                    tot += item.SubTotal();
+                }
+
+            }
+            return tot;
+
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append("Order Moment: ");
+            sb.AppendLine(Moment.ToString("dd/MM/yyyy HH:mm:ss"));
+            sb.Append("Order Status: ");
+            sb.AppendLine(Status.ToString());
+            sb.Append("Client: ");
+            sb.Append(Client.Name);
+            sb.Append(" (");
+            sb.Append(Client.BirthDate.ToString("dd/MM/yyyy"));
+            sb.Append(") - ");
+            sb.AppendLine(Client.Email);
+            sb.AppendLine("Order Itens:");
+
+            foreach (OrderItem item in Items)
+            {
+                sb.Append(item.Product.Name);
+                sb.Append(", ");
+                sb.Append(item.Price.ToString("F2", CultureInfo.InvariantCulture));
+                sb.Append(", Quantity: ");
+                sb.Append(item.Quantity.ToString());
+                sb.Append(", Sub Total: ");
+                sb.AppendLine(item.SubTotal().ToString("F2", CultureInfo.InvariantCulture));
+            }
+
+            sb.Append("Total Price:");
+            sb.AppendLine(Total().ToString("F2", CultureInfo.InvariantCulture));
+
+            return sb.ToString();
+        }
+    }
+}
+
---
+++ CursoClientePedido/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CursoClientePedido.Entities;
+using CursoClientePedido.Entities.Enums;
+using System.Globalization;
+
+namespace CursoClientePedido
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entter Client Data:");
+            Console.Write("Name:");
+            string nameCli = Console.ReadLine();
+            Console.Write("Email:");
+            string emailCli = Console.ReadLine();
+            Console.Write("Birth Date (DD/MM/YYYY):");
+            DateTime bdate = DateTime.Parse(Console.ReadLine());
+
+            Client cli = new Client(nameCli, emailCli, bdate);
+
+            Console.WriteLine("Entter Order Data:");
+            Console.Write("Status:");
+            OrderStatus st = Enum.Parse<OrderStatus>(Console.ReadLine());
+
+            Order Ord = new Order(DateTime.Now, st, cli);
+
+            Console.Write("How Many Itens To This Order?:");
+            int numItens = int.Parse(Console.ReadLine());
+
+            for (int i=1; i<=numItens; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data:");
+                Console.Write("Product Name:");
+                string nameprod = Console.ReadLine();
+                Console.Write("Product Price:");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity:");
+                int quant = int.Parse(Console.ReadLine());
+
+                Product Prod = new Product(nameprod, price);
+                OrderItem item = new OrderItem(quant, price, Prod);
+
+                Ord.AddItem(item);
+            }
+
+            Console.WriteLine("ORDER SUMARY:");
+            Console.WriteLine(Ord.ToString());
+        }
+    }
+}
